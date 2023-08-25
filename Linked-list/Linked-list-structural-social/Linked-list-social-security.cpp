790c171fe7a75f4f4d58c7d822bd35adafadc62e
Marin Grabovac <grabovacmarin000@gmail.com>
2023-08-26 00:10:48 +0200

    #1 Made Fuctions for adding and reading elements

--- Linked-list/Linked-list-structural-social/Linked-list-social-security.cpp
+++ Linked-list/Linked-list-structural-social/Linked-list-social-security.cpp
@@ -1,7 +1,53 @@
 #include <iostream>
 using namespace std;
+
+    struct maticni{
+        int mat_br;
+        char ime_prez[100];
+        int starost;
+        char spol[6];
+        maticni *sljedeci;
+    };
+
+    void dodavanje(maticni *glava){
+        maticni *zadnji, *novi;
+        zadnji = glava;
+        while(zadnji -> sljedeci)
+            zadnji = zadnji -> sljedeci;
+        novi = new maticni;
+        zadnji -> sljedeci = novi;
+        novi->sljedeci=NULL;
+        cout << "Unesite maticni broj osobe: ";
+        cin >> novi->mat_br;
+        cin.ignore();
+        cout << "Unesite ime i prezime osobe: ";
+        cin.getline(novi->ime_prez, 100);
+        cout << "Unesite starost osobe: ";
+        cin >> novi->starost;
+        cout << "Unesite spol osobe: ";
+        cin >> novi->spol;
+    }
+
+    void ispis(maticni *glava){
+        maticni *tekuci = glava->sljedeci;
+        while(tekuci){
+            cout << "Maticni broj osobe: ";
+            cout << tekuci->mat_br << endl;
+            cout << "Ime i prezime osobe: ";
+            cout << tekuci->ime_prez << endl;
+            cout << "Starost osobe: ";
+            cout << tekuci->starost << endl;
+            cout << "Spol osobe: ";
+            cout << tekuci->spol << endl;
+            cout << endl;
+            tekuci = tekuci->sljedeci;    
+        }
+    }
+
 int main(){
     int biranje;
+    maticni *glava = new maticni;
+    glava->sljedeci=NULL;
     do{
         cout << "\n1-Dodavanje novog elementa!" << endl;
         cout << "2-Ispis novog elementa!" << endl;
@@ -14,8 +60,10 @@ int main(){
         
         switch(biranje){
             case 1:
+            dodavanje(glava);
                 break;
             case 2:
+            ispis(glava);
                 break;
             case 3:
                 break;
