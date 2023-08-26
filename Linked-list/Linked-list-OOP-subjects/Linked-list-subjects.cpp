d8866cdf94012cf640a7228375be638f21bdad97
Marin Grabovac <grabovacmarin000@gmail.com>
2023-08-26 21:46:31 +0200

    #3 The first part of the task is finished

--- Linked-list/Linked-list-OOP-subjects/Linked-list-subjects.cpp
+++ Linked-list/Linked-list-OOP-subjects/Linked-list-subjects.cpp
@@ -1,6 +1,51 @@
 #include <iostream>
 using namespace std;
-int main(){
+    class kolegij{
+        private:
+        kolegij *sljedeci;
 
+        public:
+        int sif_kolegij;
+        char ime_kolegija[100];
+        int ECTS;
+        char ime_prof[100];
+
+        kolegij(){
+            sljedeci = NULL;
+        }
+    };
+
+    kolegij *objekt = new kolegij;
+int main(){
+    int biranje;
+    do{
+        cout << endl;
+        cout << "1-dodavanje novog elementa!" << endl;
+        cout << "2-ispisivanje elemenata!" << endl;
+        cout << "3-pretrazivanje elemenata!" << endl;
+        cout << "4-azuriranje elemenata!" << endl;
+        cout << "5-brisanje elemenata!" << endl;
+        cout << endl;
+        cout << "Unesite odabir: ";
+        cin >> biranje;
+        switch(biranje){
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 9:
+            cout << "Kraj programa!" << endl;
+                break;
+            default:
+            cout << "Unesite ponovno!" << endl;
+                break;
+        }
+    }while(biranje!=9);
     return 0;
 }
