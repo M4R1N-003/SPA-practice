90c28d9a78d36492c747cbd7a86d365fde02abed
Marin Grabovac <grabovacmarin000@gmail.com>
2023-08-29 20:00:48 +0200

    #9 Made functions for adding elements and reading them

--- Binary-tree/Binary tree-OOP-subject/Binary-tree-subject.cpp
+++ Binary-tree/Binary tree-OOP-subject/Binary-tree-subject.cpp
@@ -1,6 +1,115 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
+
+class binarno{
+    private:
+    binarno *lijevi;
+    binarno *desni;
+
+    public:
+    int broj;
+    char predmet[100];
+
+    binarno(){
+        lijevi=NULL;
+        desni=NULL;
+        broj = 0;
+    }
+
+    void dodavanje(int brojic, char polje[100]){
+        binarno *novi, *zadnji=this;
+        int dalje=1;
+        if(broj==0){
+            broj=brojic;
+            strcpy(predmet, polje);
+            return;
+        }
+        do{
+            if(zadnji->broj<brojic){
+                if(zadnji->desni!=NULL){
+                    zadnji=zadnji->desni;
+                }
+                else{
+                    novi = new binarno;
+                    zadnji->desni=novi;
+                    novi->broj=brojic;
+                    strcpy(novi->predmet, polje);
+                    novi->desni=NULL;
+                    novi->lijevi=NULL;
+                    dalje=0;
+                }
+            }
+            else{
+                if(zadnji->lijevi!=NULL){
+                    zadnji=zadnji->lijevi;
+                }
+                else{
+                    novi = new binarno;
+                    zadnji->lijevi=novi;
+                    novi->broj=brojic;
+                    strcpy(novi->predmet, polje);
+                    novi->lijevi=NULL;
+                    novi->desni=NULL;
+                    dalje=0;                   
+                }
+            }
+        }while(dalje==1);
+    }
+
+    void ispis(binarno *cvor){
+        if(cvor!=NULL){
+            ispis(cvor->lijevi);
+            cout << cvor->broj << " ---> " << cvor->predmet << endl;
+            ispis(cvor->desni);
+        }
+    }
+
+};
+
+binarno *objekt = new binarno;
+
 int main(){
-    
+    int biranje;
+    do{
+        cout << endl;
+        cout << "1-dodavanje novo elementa u binarno stablo!" << endl;
+        cout << "2-ispisivanje elemenata iz binarnog stabla!" << endl;
+        cout << "3-pretrazivanje elemenata binarnog stabla!" << endl;
+        cout << "4-brisanje elementa iz binarnog stabla!" << endl;
+        cout << endl;
+        cout << "Unesite odabir: ";
+        cin >> biranje;
+        switch(biranje){
+            case 1:
+            int broj1;
+            char predmet[100];
+            /*cout << "Unesite broj elementa: ";
+            cin >> broj1;
+            cin.ignore();
+            cout << "Unesite ime predmeta: ";
+            cin.getline(predmet, 100);*/
+            for(int i=-100;i<100;i++){
+                broj1=i;
+                strcpy(predmet, "Marin");
+            objekt->dodavanje(broj1, predmet);
+            }
+                break;
+            case 2:
+            objekt->ispis(objekt);
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 9:
+            cout << "Kraj programa!" << endl;
+                break;
+            default:
+            cout << "Unesite ponovno!" << endl;
+                break;
+        }
+    }while(biranje!=9);
+    return 0;
     return 0;
 }
