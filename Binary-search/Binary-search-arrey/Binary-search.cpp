2091f01bdfac0ee35cc6d2500049a14f14b3a626
Marin Grabovac <grabovacmarin000@gmail.com>
2023-09-20 14:31:31 +0200

    #37 Made exchange sort

--- Binary-search/Binary-search-arrey/Binary-search.cpp
+++ Binary-search/Binary-search-arrey/Binary-search.cpp
@@ -9,5 +9,17 @@ int main(){
         cout << "Unesite " << i+1 << ". broj: ";
         cin >> polje[i];
     }
+
+    for(int i=N-1;i>=0;i--){
+        for(int j=0;j<i;j++){
+            if(polje[j]>polje[i]){
+                int temp;
+                temp = polje[j];
+                polje[j] = polje[i];
+                polje[i]=temp;
+            }
+        }
+    }
+    
     return 0;
 }
