c37d862e96cdb28c6798fb116b6c3b0c354d1fdd
Marin Grabovac <grabovacmarin000@gmail.com>
2023-09-30 23:34:43 +0200

    #49 Finished the issue

--- Quick-sort/Quick-sort-arrey/Quick-sort.cpp
+++ Quick-sort/Quick-sort-arrey/Quick-sort.cpp
@@ -1,5 +1,39 @@
 #include <iostream>
 using namespace std;
+
+int RazmjestiElem(int nizElem[], int donja, int gornja);
+void Zamijeni(int nizElem[], int i, int j);
+
+void Quicksort(int nizElem[], int donja, int gornja){
+    if (gornja <= donja) return;
+    int j = RazmjestiElem(nizElem, donja, gornja);
+    Quicksort(nizElem, donja, j - 1); 
+    Quicksort(nizElem, j + 1, gornja); 
+}
+
+int RazmjestiElem(int nizElem[], int donja, int gornja){
+    int i = donja, j = gornja + 1;
+    int pivot = nizElem[donja];
+    while (true)
+    {
+        while (nizElem[++i] < pivot)
+            if (i == gornja) break;
+        while (pivot < nizElem[--j])
+        { /*if (j == donja) break;*/ }
+        if (i >= j) break;
+        Zamijeni(nizElem, i, j);
+    }
+    Zamijeni(nizElem, donja, j);
+    return j;
+}
+
+void Zamijeni(int nizElem[], int i, int j){
+    int prvr = nizElem[i];
+    nizElem[i] = nizElem[j]; 
+    nizElem[j] = prvr; 
+}
+
+
 int main(){
     int n;
     cout << "Unesite broj brojeva: ";
@@ -14,11 +48,14 @@ int main(){
     cin >> k;
     for(int i=0;i<n;i++){
         if(polje[i]%k==0){
-            polje[i]=-9371635234;
+            polje[i]=-93716352;
         }
     }
+
+    Quicksort(polje, 0, n - 1);
+
     for(int i=0;i<n;i++){
-        if(polje[i]!=-9371635234){
+        if(polje[i]!=-93716352){
             cout << polje[i] << " ";
         }
     }
